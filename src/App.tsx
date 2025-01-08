d155adc1e21e4234ae123d269692d43dc72941ed
Miguel Gresa <95084763+KaruzG@users.noreply.github.com>
2025-01-08 23:00:16 +0000

    Tailwind local colors

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg' */
 import LandingSection from './components/LandingSection';
 import MySkillsSection from './components/MySkillsSection';
-import NavBar from './components/NavBar';
+/* import NavBar from './components/NavBar'; */
 import ProjectsSection from './components/ProjectsSection';
 
 function App() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '',
+        secondary: '#C9C9C9',
+        accent: '#00A933',
+        titleWhite: '#FFFFFF',
+      }
+    },
   },
   plugins: [],
 }
