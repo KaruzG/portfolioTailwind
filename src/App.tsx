36c69b253cf8ee42af85f55bf8334b7c4875b8ed
KaruzG <josemiguelcpcs@gmail.com>
2025-03-05 23:03:38 +0100

    refactor: Remove unused imports in App and ContactButton components for cleaner code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-/* import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg' */
 import ContactButton from "./components/ContactButton";
 import ExperienceSection from "./components/ExperienceSection";
 import LandingSection from "./components/LandingSection";
--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -1,6 +1,5 @@
 import { CiMail } from "react-icons/ci";
 
-
 const ContactButton = () => {
   return (
     <button className="flex justify-center m-auto my-8 rounded-full bg-accent px-14 py-4 text-xl text-white font-semibold">
