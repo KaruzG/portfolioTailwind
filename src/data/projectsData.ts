ecbe79d28d93f78122e602f7e8e575f6a6baa59f
KaruzG <josemiguelcpcs@gmail.com>
2025-03-05 22:59:23 +0100

    fix: Update project image paths to use imported assets for consistency

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,10 +1,12 @@
 import { Project } from "../types/Project";
 import { skills } from "./skillsData";
+import skateSpotsVLC from "../assets/projects/skatespotsvlc.jpeg";
+import karuzFilm from "../assets/projects/karuzfilm.png";
 
 export const projects: Project[] = [
   {
     name: "SKATESPOTSVLC",
-    img: "/src/assets/projects/skatespotsvlc.jpeg",
+    img: skateSpotsVLC,
     description: "Web app with info about my city and an admin panel to upload new markers with photos and descriptions",
     techStack: [skills[0],skills[2],skills[6],skills[8],skills[9],skills[10],skills[11]],
     github: "https://github.com/KaruzG/skatespotsvlc",
@@ -12,7 +14,7 @@ export const projects: Project[] = [
   },
   {
     name: "KARUZFILM",
-    img: "/src/assets/projects/karuzfilm.png",
+    img: karuzFilm,
     description: "Photography portfolio",
     techStack: [skills[15]],
     github: null,
