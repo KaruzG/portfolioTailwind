282263dab1f83ba85cc329817740bcf8c4251d62
KaruzG <josemiguelcpcs@gmail.com>
2025-03-05 23:15:23 +0100

    fix: Change ContactButton to use a mailto link for direct email access

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -2,9 +2,11 @@ import { CiMail } from "react-icons/ci";
 
 const ContactButton = () => {
   return (
-    <button className="flex justify-center m-auto my-8 rounded-full bg-accent px-14 py-4 text-xl text-white font-semibold">
-      <CiMail className="mr-3 my-auto" size={26} style={{ strokeWidth: 1 }}/> Contact
-    </button>
+    <a className="flex justify-center m-auto" href="mailto:your-email@gmail.com">
+        <button className="flex justify-center m-auto my-8 rounded-full bg-accent px-14 py-4 text-xl text-white font-semibold">
+          <CiMail className="mr-3 my-auto" size={26} style={{ strokeWidth: 1 }}/> Contact
+        </button>
+    </a>
   );
 };
 
