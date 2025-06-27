766ccbfe8a50bcb5dd01006a1da33bda1eead67f
Miguel Gresa <josemiguelcpcs@gmail.com>
2025-06-27 09:36:33 +0200

    feat(ProjectSection): Better desktop sizes

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -3,15 +3,19 @@ import ProjectCard from "./ProjectCard";
 import SectionTitle from "./SectionTitle";
 import { projects } from "../data/projectsData";
 
-
 const ProjectsSection = () => {
   return (
-    <section id="projects" className="mb-16">
+    <section
+      id="projects"
+      className="relative left-1/2 mb-16 w-screen -translate-x-1/2"
+    >
+      {" "}
+      {/* Little hack to break the max-w of app component */}
       <SectionTitle>
         <FaRegFolder className="my-auto mr-3" />
         PROJECTS
       </SectionTitle>
-      <ul className="mt-5 flex flex-col gap-3">
+      <ul className="mt-5 flex flex-col gap-3 sm:mx-auto sm:grid sm:grid-cols-2 sm:gap-3 lg:max-w-[1100px] lg:grid-cols-3">
         {projects.map((project) => (
           <ProjectCard key={project.name} project={project} />
         ))}
