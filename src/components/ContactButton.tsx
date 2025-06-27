fe1444c8314f5fa8e493684b5c44902db7f5b90d
Miguel Gresa <josemiguelcpcs@gmail.com>
2025-06-27 11:37:01 +0200

    fix(ContactButton): Update email link to LinkedIn profile for improved contact accessibility

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -2,7 +2,7 @@ import { CiMail } from "react-icons/ci";
 
 const ContactButton = () => {
   return (
-    <a className="flex justify-center m-auto" href="mailto:your-email@gmail.com">
+    <a className="flex justify-center m-auto" href="https://www.linkedin.com/in/josemiguelgresa/">
         <button className="flex justify-center m-auto my-8 rounded-full bg-accent px-14 py-4 text-xl text-white font-semibold hover:bg-accent300 transition-colors">
           <CiMail className="mr-3 my-auto" size={26} style={{ strokeWidth: 1 }}/> Contact
         </button>
