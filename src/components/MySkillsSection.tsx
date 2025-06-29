bf7d8ca7685d519c5b8899ad962e5ed6f6c96dbf
Miguel Gresa <josemiguelcpcs@gmail.com>
2025-06-29 23:29:15 +0200

    feat:(SkillsSection): Height animation for skills cards

--- src/components/MySkillsSection.tsx
+++ src/components/MySkillsSection.tsx
@@ -3,13 +3,21 @@ import { FaRegStar } from "react-icons/fa";
 import SkillCard from "./SkillCard";
 import { SkillType } from "../types/Skill";
 import { skills } from "../data/skillsData";
-import { useState } from "react";
+import { useRef, useLayoutEffect, useState } from "react";
 
 const MySkillsSection = () => {
   const [activeSection, setActiveSection] = useState<SkillType>("frontend");
   const filteredSkills = skills.filter(
     (skill) => skill.techType === activeSection,
   );
+  const [height, setHeight] = useState<number>();
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (contentRef.current) {
+      setHeight(contentRef.current.offsetHeight);
+    }
+  }, [activeSection, filteredSkills.length]);
 
   return (
     <section id="skills" className="mb-16">
@@ -20,29 +28,37 @@ const MySkillsSection = () => {
       <nav className="mt-5">
         <ul className="flex justify-center gap-3 font-bold text-grayButton">
           <li
-            className={`transition-colors duration-500 cursor-pointer ${activeSection == "frontend" ? "text-accent" : ""}`}
+            className={`cursor-pointer transition-colors duration-500 ${activeSection == "frontend" ? "text-accent" : ""}`}
             onClick={() => setActiveSection("frontend")}
           >
             Front End
           </li>
           <li
-            className={`transition-colors duration-500 cursor-pointer ${activeSection == "backend" ? "text-accent" : ""}`}
+            className={`cursor-pointer transition-colors duration-500 ${activeSection == "backend" ? "text-accent" : ""}`}
             onClick={() => setActiveSection("backend")}
           >
             Back End
           </li>
           <li
-            className={`transition-colors duration-500 cursor-pointer ${activeSection == "other" ? "text-accent" : ""}`}
+            className={`cursor-pointer transition-colors duration-500 ${activeSection == "other" ? "text-accent" : ""}`}
             onClick={() => setActiveSection("other")}
           >
             Other
           </li>
         </ul>
       </nav>
-      <div className="flex-wrap justify-center gap-4 mt-10 flex md:max-w-md md:mx-auto">
-        {filteredSkills.map((skill) => (
-          <SkillCard key={skill.name} skill={skill} />
-        ))}
+      <div
+        className="md:max-w-md mx-auto relative overflow-visible transition-all duration-500"
+        style={{ height: height ? `${height}px` : "auto" }}
+      >
+        <div
+          ref={contentRef}
+          className="mt-10 flex flex-wrap justify-center gap-4"
+        >
+          {filteredSkills.map((skill) => (
+            <SkillCard key={skill.name} skill={skill} />
+          ))}
+        </div>
       </div>
     </section>
   );
