a2a53d725f6acc0be1312ff45a175783bd374ae4
Miguel Gresa <josemiguelcpcs@gmail.com>
2025-07-21 20:27:45 +0200

    refactor(experienceSection): Added experienceData.ts

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -1,43 +1,7 @@
 import SectionTitle from "./SectionTitle";
 import { FiPaperclip } from "react-icons/fi";
 import Timeline from "./Timeline";
-import { TimelineData } from "../types/TimelineData";
-
-const jobExperience: Array<TimelineData> = [
-  {
-    company: "Code Systems",
-    location: "Remote",
-    duration: 4,
-    durationType: "months",
-    descriptionList: [
-      "<b>FullStack development</b> of a CRM with IOT connection",
-      "Deployment and <b>SQL Databases</b>",
-      "<b>UIX Design</b>"
-    ],
-  },
-  {
-    company: "CiviRed",
-    location: "Spain",
-    duration: 1,
-    durationType: "year",
-    descriptionList: [
-      "<b>System Administration</b>",
-      "<b>Coordination</b> of the IT for congresses",
-      "Deployment of <b>servers and networks</b>",
-    ],
-  },
-  {
-    company: "Bartleby&Co",
-    location: "Germany",
-    duration: 3,
-    durationType: "months",
-    descriptionList: [
-      "IT Technical Support",
-      "<b>Wordpress</b> administrator",
-      "Web and Databases <b>backups</b>",
-    ],
-  },
-];
+import { experienceData } from "../data/experienceData";
 
 const ExperienceSection = () => {
   return (
@@ -47,7 +11,7 @@ const ExperienceSection = () => {
         EXPERIENCE
       </SectionTitle>
       <ul className="ml-3 mt-5 border-l-2">
-        <Timeline timelineData={jobExperience} type="job" />
+        <Timeline timelineData={experienceData} type="job" />
       </ul>
     </section>
   );
---
+++ src/data/experienceData.ts
@@ -0,0 +1,37 @@
+import { TimelineData } from "../types/TimelineData";
+
+export const experienceData: Array<TimelineData> = [
+  {
+    company: "Code Systems",
+    location: "Remote",
+    duration: 4,
+    durationType: "months",
+    descriptionList: [
+      "<b>FullStack development</b> of a CRM with IOT connection",
+      "Deployment and <b>SQL Databases</b>",
+      "<b>UIX Design</b>"
+    ],
+  },
+  {
+    company: "CiviRed",
+    location: "Spain",
+    duration: 1,
+    durationType: "year",
+    descriptionList: [
+      "<b>System Administration</b>",
+      "<b>Coordination</b> of the IT for congresses",
+      "Deployment of <b>servers and networks</b>",
+    ],
+  },
+  {
+    company: "Bartleby&Co",
+    location: "Germany",
+    duration: 3,
+    durationType: "months",
+    descriptionList: [
+      "IT Technical Support",
+      "<b>Wordpress</b> administrator",
+      "Web and Databases <b>backups</b>",
+    ],
+  },
+];
