320445435e9d206352a80c7a0f7078d50754a3d2
Miguel Gresa <josemiguelcpcs@gmail.com>
2025-08-11 16:47:31 +0200

    fix(scrollToSection): Added different margin offsets for desktop and mobile

--- src/hooks/useScrollToSection.tsx
+++ src/hooks/useScrollToSection.tsx
@@ -1,12 +1,15 @@
 export const scrollToSection = (id: string) => {
-    const section = document.getElementById(id);
+    const section = document.getElementById(id)
+    const MARGIN_DESKTOP = 100
+    const MARGIN_MOBILE = 30
+
     if (section) {
       window.scrollTo({
         behavior: "smooth",
         top:
           section.getBoundingClientRect().top -
           document.body.getBoundingClientRect().top -
-          30,
+          (window.innerWidth >= 768 ? MARGIN_DESKTOP : MARGIN_MOBILE),
       });
     }
   };
